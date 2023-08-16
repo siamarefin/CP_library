a96d397a0676e57411215dfd150fd7e1cb6d2046
siamarefin <93789655+siamarefin@users.noreply.github.com>
2023-08-16 16:39:50 +0600

    Update graph_bipartite_graph .cpp

--- c_programming library/graph/graph_bipartite_graph .cpp
+++ c_programming library/graph/graph_bipartite_graph .cpp
@@ -62,3 +62,95 @@ int main()
 
     return 0;
 }
+
+
+
+
+// apna clg code 
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int 
+#define cin(n) ll n ; cin>>n;
+#define cout(n) cout<<n;
+#define for_loop(i, j, n) for (ll i = j; i <= n; i++)
+#define st(v) sort(v.begin(), v.end());
+#define sz(x) (ll) x.size()
+#define no cout << "NO\n";
+#define yes cout << "YES\n";
+#define newline cout<<"\n";
+#define space cout<<" ";
+
+std::vector<std::vector<int > > adj;
+std::vector<bool> vis;
+std::vector<int > col;
+
+bool bipart ;
+
+void color(int u , int curr)
+{
+    if(col[u] != - 1 && col[u]!=curr)
+    {
+        bipart= false ;
+        return;
+    }
+    col[u] = curr;
+    if(vis[u])
+        return;
+     vis[u]= true;
+     for(auto i : adj[u])
+     {
+        color(i,curr^1);
+     }
+}
+
+ 
+int main()
+{
+    bipart= true;
+    int n , m ;
+    cin>>n>>m;
+    adj= std::vector<std::vector<int >> (n);
+    vis=std::vector<bool> (n,false);
+    col=std::vector<int > (n,-1);
+
+    for(int i =0 ; i<m;i++)
+    {
+        int u , v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+
+        adj[v].push_back(u);
+
+    }
+
+
+    for(int i =0  ;i<n;i++)
+    {
+        if(!vis[i])
+        {
+            color(i,0);
+        }
+    }
+
+    if(bipart)
+        yes
+    else
+        no
+    newline
+    
+
+
+
+    return 0;
+}
+
+/*
+4 4
+0 1
+1 2
+2 3
+3 0
+*/

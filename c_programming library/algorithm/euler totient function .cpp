88020cfa3864a3e008b2291fdf5ca614af737065
siamarefin <93789655+siamarefin@users.noreply.github.com>
2023-11-24 07:10:14 +0600

    Create euler totient function .cpp

---
+++ c_programming library/algorithm/euler totient function .cpp
@@ -0,0 +1,13 @@
+/*
+Φ(n) = Number of positive integers less than 'n' that are relatively prime to n.
+
+gcd ==1  hole relatively prime hoi 
+
+Φ(5) = 4 
+karon 1,2,3,4 sob gular sathe 5 er gcd == 1 
+
+Φ(8) = 4 
+karon 1,3,5,7 er sathe 8 er gcd ==1 
+
+
+*/ 

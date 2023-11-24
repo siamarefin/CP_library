286d7cd16033271a1abe6ded34d2f0b018cd0ce2
siamarefin <93789655+siamarefin@users.noreply.github.com>
2023-11-24 18:37:30 +0600

    Create Chinese Remainder Theorem.cpp

---
+++ c_programming library/algorithm/Chinese Remainder Theorem.cpp
@@ -0,0 +1,10 @@
+/* The Chinese Remainder Theorem (CRT) is used to solve a set of different congruent equations with one variable 
+but different moduli which are relatively prime as shown below: 
+
+congruent : Two integers a and b are said to be congruent modulo m if their difference a–b is divisible by the integer m.
+It is then said that a is congruent to b modulo m, and this statement is written in the symbolic form a≡b (mod m). 
+Such a relation is called a congruence.
+
+
+
+*/ 

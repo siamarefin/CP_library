12e942953fdd3bd5f566c1a1b68ec30f9a17cd7d
siamarefin <93789655+siamarefin@users.noreply.github.com>
2023-11-24 06:29:11 +0600

    Create Fermat’s little theorem.cpp

---
+++ c_programming library/algorithm/Fermat’s little theorem.cpp
@@ -0,0 +1,31 @@
+// https://www.geeksforgeeks.org/fermats-little-theorem/
+/*   Fermat’s little theorem states that if p is a prime number, then for any integer a, the number a p – a is an integer multiple of p. 
+Use of Fermat’s little theorem: If we know m is prime, then we can also use Fermat’s little theorem to find the inverse. */ 
+
+#include <bits/stdc++.h>
+using namespace std;
+int power(int x, unsigned int y, unsigned int m);
+void modInverse(int a, int m)
+{
+	if (__gcd(a, m) != 1)
+		cout << "Inverse doesn't exist";
+	else {
+		cout << "Modular multiplicative inverse is "
+			<< power(a, m - 2, m);
+	}
+}
+int power(int x, unsigned int y, unsigned int m)
+{
+	if (y == 0)
+		return 1;
+	int p = power(x, y / 2, m) % m;
+	p = (p * p) % m;
+
+	return (y % 2 == 0) ? p : (x * p) % m;
+}
+int main()
+{
+	int a = 3, m = 11;
+	modInverse(a, m);
+	return 0;
+}

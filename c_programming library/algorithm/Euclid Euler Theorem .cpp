9645d1282c585e55300307c6417d1aed9699d6a9
siamarefin <93789655+siamarefin@users.noreply.github.com>
2023-11-24 07:04:19 +0600

    Create Euclid Euler Theorem .cpp

---
+++ c_programming library/algorithm/Euclid Euler Theorem .cpp
@@ -0,0 +1,14 @@
+/* For every positive integer 'a' & 'n', which are said to be  relatively prime, then a^Ф(n) = I mod n. 
+Euler's Theorem
+Example 1: Prove Euler's theorem hold true for a=3 and n=10.
+Solution:
+Given: a=3 and n=10.
+a(n) = 1 (mod n)
+39(10) = 1 (mod 10)
+Φ(10) = 4 // according to euler totient function 
+34 = 1 (mod 10)
+81 = 1 (mod 10)
+Therefore, Euler's theorem holds true for a=3 and n=10.
+
+
+*/ 
